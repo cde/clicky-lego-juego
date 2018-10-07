044bdeaf5ac06db9dce80c370c93896341ff121b
Carmen Diaz <cdechauri@gmail.com>
2018-10-06 23:30:17 -0700

    Updates

---
+++ precache-manifest.fc9303b95216a6c6fb79fe36c2b99e44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb1b1596fce17aaaf408",
+    "url": "/static/js/main.bb1b1596.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "bb1b1596fce17aaaf408",
+    "url": "/static/css/main.2c3eaef9.chunk.css"
+  },
+  {
+    "revision": "99f35ffa28c67eedecaf16c22d1cec4e",
+    "url": "/index.html"
+  }
+];
